ad7afba9cf12babe5ab506d81672585b5c7b679c
Bruno Lange <blangeram@gmail.com>
2019-06-19 21:11:46 -0700

    Submits reduce-based solution to PS 1.3

--- Chapter01/1.3/solution.py
+++ Chapter01/1.3/solution.py
@@ -22,7 +22,24 @@ def maximum_circular_subarray(arr):
     # solution is either in the normal case or the wraparound one
     return max(maximum_subarray(arr), sum(arr) - minimum_subarray(arr))
 
+from functools import reduce
+
+def ms_reduce(arr):
+    """ reduce the arr down to the solution
+    At each iteration, we pass along the current values
+    for max_local and max_global
+    """
+    def r(acc, curr):
+        max_local, max_global = acc
+        max_local = max(max_local + curr, curr)
+        max_global = max(max_global, max_local)
+        return max_local, max_global
+
+    _, max_global = reduce(r, arr, (float('-inf'), float('-inf')))
+    return max_global
+
 if __name__ == '__main__':
     assert maximum_subarray([34, -50, 42, 14, -5, 86]) == 137
+    assert ms_reduce([34, -50, 42, 14, -5, 86]) == 137
     assert maximum_subarray([-1,-2,-3]) == -1
     assert maximum_subarray([]) == float('-inf')
